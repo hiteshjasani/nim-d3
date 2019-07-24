0e02a31bfe1f5c3a37cfe85f327c52e8a5033f6a
Hitesh Jasani <hitesh.jasani@gmail.com>
2019-07-23 20:14:31 -0400

    Expand linear scale examples

--- examples/src/pages/ex3.nim
+++ examples/src/pages/ex3.nim
@@ -6,10 +6,14 @@ const
 var
   scaleLin: D3ContinuousScale
   scaleLinR: D3ContinuousScale
+  scaleLinColor: D3ContinuousScale
+  scalePieces: D3ContinuousScale
 
 proc runOnce() =
   scaleLin = scaleLinear().continuousDomain(0, 100).continuousRange(200, 500)
   scaleLinR = scaleLinear().continuousDomain(0, 100).continuousRange(500, 200)
+  scaleLinColor = scaleLinear().continuousDomain(0, 100).continuousRange("brown", "steelblue")
+  scalePieces = scaleLinear().continuousDomain(@[-1, 0, 1]).continuousRange(@["red", "white", "blue"])
 
 proc createDom(): VNode =
   result = buildHtml(tdiv):
@@ -37,11 +41,30 @@ proc createDom(): VNode =
             td: text "0 - 100"
             td: text "500 - 200"
             td(id = "scaleLinR"): text "-"
+          tr:
+            td: text $orig1
+            td: text "0 - 100"
+            td: text "brown - steelblue"
+            td(id = "scaleLinColor"): text "-"
+          tr:
+            td: text "-0.5"
+            td: text "-1, 0, 1"
+            td: text "red, white, blue"
+            td(id = "scalePieces1"): text "-"
+          tr:
+            td: text "0.5"
+            td: text "-1, 0, 1"
+            td: text "red, white, blue"
+            td(id = "scalePieces2"): text "-"
     button(`type` = "button"):
       text "Scale values"
       proc onclick(ev: Event, n: VNode) =
         discard select("#testarea #scaleLin").text($exec(scaleLin, orig1))
         discard select("#testarea #scaleLinR").text($exec(scaleLinR, orig1))
+        discard select("#testarea #scaleLinColor").text($exec(scaleLinColor,
+                                                              orig1))
+        discard select("#testarea #scalePieces1").text($exec(scalePieces, -0.5))
+        discard select("#testarea #scalePieces2").text($exec(scalePieces, 0.5))
 
 setRenderer createDom
 setForeignNodeId("testarea")
--- src/d3.nim
+++ src/d3.nim
@@ -1,4 +1,4 @@
-import jsbind
+import jsbind, sequtils
 
 type
   D3Selection* {.importc.} = ref object of JSObj
@@ -21,5 +21,14 @@ proc text*(node: D3Selection, fn: proc(x: int): cstring): D3Selection {.importcp
 
 proc scaleLinear*(): D3ContinuousScale {.jsimportgWithName: "d3.scaleLinear".}
 proc exec*(scale: D3ContinuousScale, v: int): int {.jsimportgWithName: "function(scale, v) {return scale(v)}"}
+proc exec*(scale: D3ContinuousScale, v: float): float {.jsimportgWithName: "function(scale, v) {return scale(v)}"}
 proc continuousDomain*(scale: D3ContinuousScale; minVal, maxVal: int): D3ContinuousScale {.jsimportgWithName: "function(scale, minVal, maxVal){ return scale.domain([minVal, maxVal])}".}
+proc continuousDomain*(scale: D3ContinuousScale; extents: seq[int]): D3ContinuousScale {.jsimportgWithName: "function(scale, extents){ return scale.domain(extents)}".}
 proc continuousRange*(scale: D3ContinuousScale; minVal, maxVal: int): D3ContinuousScale {.jsimportgWithName: "function(scale, minVal, maxVal){return scale.range([minVal, maxVal])}".}
+proc continuousRange*(scale: D3ContinuousScale; extents: seq[int]): D3ContinuousScale {.jsimportgWithName: "function(scale, extents){return scale.range(extents)}".}
+proc continuousRange*(scale: D3ContinuousScale; minVal, maxVal: cstring): D3ContinuousScale {.jsimportgWithName: "function(scale, minVal, maxVal){return scale.range([minVal, maxVal])}".}
+proc continuousRange*(scale: D3ContinuousScale; extents: seq[cstring]): D3ContinuousScale {.jsimportgWithName: "function(scale, extents){return scale.range(extents)}".}
+proc continuousRange*(scale: D3ContinuousScale;
+                      extents: seq[string]): D3ContinuousScale =
+  continuousRange(scale, map(extents, proc(x: string): cstring =
+                                          cstring(x)))
