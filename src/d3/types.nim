09290aadb6fcd6e20e60674645b3c3a8d46de8ed
Hitesh Jasani <hitesh.jasani@gmail.com>
2019-07-25 19:47:33 -0400

    Remove unnecessary pragma

--- src/d3/types.nim
+++ src/d3/types.nim
@@ -1,6 +1,6 @@
 type
   D3Event* = ref object
-  D3Selection* {.importc.} = ref object
+  D3Selection* = ref object
   D3ContinuousScale* = ref object
   D3OrdinalScale* = ref object
   D3Axis* = ref object
