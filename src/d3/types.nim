dbbede50f851e644466030677f2635929eb8cfa1
Hitesh Jasani <hitesh.jasani@gmail.com>
2019-07-25 20:04:42 -0400

    Add docs

--- src/d3/types.nim
+++ src/d3/types.nim
@@ -1,6 +1,9 @@
 type
   D3Event* = ref object
   D3Selection* = ref object
-  D3ContinuousScale* = ref object
-  D3OrdinalScale* = ref object
-  D3Axis* = ref object
+  D3ContinuousScale* = ref object ## \
+    ## Continuous scales include linear, power, log, identity, time
+  D3OrdinalScale* = ref object ## \
+    ## Ordinal scales include band, point
+  D3Axis* = ref object ## \
+    ## Representing a coordinate axis
